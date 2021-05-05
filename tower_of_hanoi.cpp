8829c556e3befaee54d04629cca802f36aa62802
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-05-05 09:50:44 +0530

    Create tower_of_hanoi.cpp
    Returns a vector of vectors with sequential solution to tower of hanoi problem. Here, in every sub vector: first element indicates the *from* rod while second indicates the *target* rod.

---
+++ tower_of_hanoi.cpp
@@ -0,0 +1,29 @@
+std::vector <vector <int>> ans;
+
+// from, aux, target
+void toh(int n, int f, int a, int t) {
+    // base case
+    if (n==1) {
+        // move the disc f to t
+        vector <int> temp {f, t};
+        ans.push_back(temp);
+        return;
+    }
+    
+    toh(n-1, f, t, a);
+    
+    // move the last disc
+    vector <int> temp {f, t};
+    ans.push_back(temp);
+    
+    toh(n-1, a, f, t);
+}
+
+
+vector<vector<int>> towerOfHanoi(int n)
+{
+    
+    ans.clear();
+    toh(n, 1, 2, 3);   
+	return ans;
+}

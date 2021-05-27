8d7c3e458b00830d3d0a5554e95f665fd9ec8a5b
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-05-27 23:24:12 +0530

    Update buy_sell_stock.cpp

--- miscellaneous/buy_sell_stock.cpp
+++ miscellaneous/buy_sell_stock.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0;
+        int ans = 0, m=0;
         int n = prices.size();
-        std::vector<int> v(n+1);
-        for (int i=n-1; i>=0; --i) 
-            v[i] = std::max(v[i+1], prices[i]);
-        for (int i=0; i<n; ++i) {
-            ans = std::max(ans, v[i+1]-prices[i]);
+        for (int i=n-1; i>0; --i) {
+            m = std::max(prices[i], m);
+            ans = std::max(ans, m-prices[i-1]);
         }
         return ans;
     }

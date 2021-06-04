531bceb466d0e511f88bfb6bd01651bd380af527
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-06-04 23:24:36 +0530

    Create move zeroes.cpp

---
+++ miscellaneous/move zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector <int> v;
+        
+        for (int i=0; i<nums.size(); ++i)
+            if (!nums[i]) v.push_back(i);
+        
+        int z = 0;
+        
+        for (int x: v) {
+            nums.erase(nums.begin() + x - z);
+            z++;
+        }
+        
+        for (int i = 0; i<v.size(); ++i) nums.push_back(0);
+        
+    }
+};

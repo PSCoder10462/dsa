a052e8b3942cb2c9adbef34d2811e1f565263252
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-06-06 23:20:01 +0530

    Create CLEANUP.cpp

---
+++ codechef/practice/CLEANUP.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) (l + h / 2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  vi v(n);
+  loop(i, 1, m) {
+    int t;
+    cin >> t;
+    v[t - 1] = 1;
+  }
+  bool c = true;
+  vi ch, as;
+  loop(i, 0, n - 1) {
+    if (not v[i]) {
+      c ? ch.push_back(i + 1) : as.push_back(i + 1);
+      c = not c;
+    }
+  }
+
+  for (int i : ch) cout << i << " ";
+  cout << endl;
+  for (int i : as) cout << i << " ";
+  cout << endl;
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

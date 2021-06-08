64c8349b6d79e347d734d2083e6ba7fb3453931c
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-06-08 23:11:05 +0530

    Create CHFHEIST.cpp

---
+++ codechef/long challenges/june 2021/CHFHEIST.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) (l + h / 2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  int D, d, p, q;
+  cin >> D >> d >> p >> q;
+
+  int n = D / d;
+
+  int a1 = ((n) * ((2 * p) + ((n - 1) * q))) / 2;
+
+  int rem = D % d;
+
+  int x = p + (n * q);
+
+  cout << (a1 * d) + (x * rem) << endl;
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

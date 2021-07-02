4ae51fc7d620968e15ca99ef4b92eea3e71f0b0a
Himesh Nayak <30944790+HimeshNayak@users.noreply.github.com>
2021-07-02 22:42:42 +0530

    Create Ques1.java

---
+++ java/Ques1.java
@@ -0,0 +1,10 @@
+/**
+    @author Himesh Nayak 01216403219
+    Write a java program to print hello word.
+ */
+
+ class Ques1 {
+    public static void main (String[] args) {
+        System.out.println("hello world");
+    }
+ }

53254a84eb164ecd9a143f9c85c892dcf5ed45d1
Himesh Nayak <30944790+HimeshNayak@users.noreply.github.com>
2021-07-03 22:48:29 +0530

    Create Ques6.java

---
+++ java/Ques6.java
@@ -0,0 +1,12 @@
+/**
+    @author Himesh Nayak 01216403219
+    Write a java program to convert integer to decimal.
+ */
+
+ class Ques6 {
+     public static void main(String[] args) {
+         int integer = 5;
+         double decimal = (double)integer;
+         System.out.println("Integer: " + integer + " to float : " + decimal);
+     }
+ }

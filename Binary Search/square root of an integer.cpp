d083d14931d61e890e239523754d15c81ea87dca
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-07-07 07:02:16 +0530

    Create square root of an integer.cpp

---
+++ Binary Search/square root of an integer.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) ((l + h) / 2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define mod(a) (a % 100'000'007)
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+int findRoot(int l, int h, int n) {
+  int m = mid(l, h);
+  if ((m * m == n) or (m * m < n and (m + 1) * (m + 1) > n)) return m;
+
+  if (m * m > n) return findRoot(l, m - 1, n);
+  return findRoot(m + 1, h, n);
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  cout << findRoot(1, n, n) << endl;
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

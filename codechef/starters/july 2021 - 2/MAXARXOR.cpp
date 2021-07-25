7bbe76d5812872030bda9c286b5789700aa9fec6
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-07-25 17:50:08 +0530

    Create MAXARXOR.cpp

---
+++ codechef/starters/july 2021 - 2/MAXARXOR.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) ((l + h) / 2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define mod(a) (a % 100'000'007)
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  int j = 1;
+  loop(i, 0, n - 1) j <<= 1ll;
+  j -= 1;
+  int x = min(k, (j + 1) / 2);
+  cout << j * x * 2 << endl;
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

364dc19f4558c8c84e4faf09a56da8c5f15c4e89
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-07-25 16:05:16 +0530

    Create CHSFORMT.cpp

---
+++ codechef/starters/july 2021 - 2/CHSFORMT.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) ((l + h) / 2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define mod(a) (a % 100'000'007)
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  int s = a + b;
+  if (s < 3)
+    cout << 1;
+  else if (s < 11)
+    cout << 2;
+  else if (s < 61)
+    cout << 3;
+  else
+    cout << 4;
+  cout << endl;
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T;
+  std::cin >> T;
+  while (T--) {
+    solve();
+  }
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

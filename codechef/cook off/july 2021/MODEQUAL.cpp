b9593904ed04d98ab225cfb476dce4cafa9f38f6
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-07-28 21:23:44 +0530

    Create MODEQUAL.cpp

---
+++ codechef/cook off/july 2021/MODEQUAL.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) ((l + h) / 2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define mod(a) (a % 100'000'007)
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  vi v(n);
+  loop(i, 0, n - 1) cin >> v[i];
+  sort(v.begin(), v.end());
+  int e = v[0];
+  int op = 0;
+  int i = 0;
+  while (i < n and v[i] == e) i++;
+  while (i < n) {
+    if (v[i] % (v[i] - e) != e) {
+      cout << n << endl;
+      return;
+    }
+    op++;
+    i++;
+  }
+  cout << op << endl;
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

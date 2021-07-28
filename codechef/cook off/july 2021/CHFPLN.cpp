2c80e7688e72fb26129d9959408a71b059f9bb89
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-07-28 21:06:25 +0530

    Create CHFPLN.cpp

---
+++ codechef/cook off/july 2021/CHFPLN.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) ((l + h) / 2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define mod(a) (a % 100'000'007)
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  // code goes here
+  int n;
+  cin >> n;
+  vi v(n);
+  vi f;
+  int fs = 0;
+  loop(i, 0, n - 1) {
+    cin >> v[i];
+    if (f.size() <= v[i]) {
+      f.resize(v[i] + 1, 0);
+    }
+    fs = max(fs, v[i]);
+    f[v[i]]++;
+  }
+  int ans = 0;
+  loop(i, 1, fs) { ans += min(i - 1, f[i]); }
+  cout << ans << endl;
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

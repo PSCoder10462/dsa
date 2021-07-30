1f3e62c874badd75f6041207f469ff57d4073b6c
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-07-30 13:15:41 +0530

    Create 21_isYourHorseShoeOnTheOtherHoof.cpp

---
+++ A2OJ/Ladder 11 (<1300)/21_isYourHorseShoeOnTheOtherHoof.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) ((l+h)/2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define MOD 100'000'007
+#define mp make_pair
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  unordered_set<int> s;
+  loop(i, 0, 3) {
+  	int n;
+  	cin >> n;
+  	s.insert(n);
+  }
+  cout << 4-s.size();
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  solve();
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

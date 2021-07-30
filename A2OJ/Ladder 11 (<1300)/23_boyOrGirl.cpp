fb4f55cf5e21d2465d0face6c95f879244a619fd
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-07-30 13:25:08 +0530

    Create 23_boyOrGirl.cpp

---
+++ A2OJ/Ladder 11 (<1300)/23_boyOrGirl.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) ((l+h)/2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define MOD 100'000'007
+#define mp make_pair
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  string username;
+  cin >> username;
+  unordered_set<char> s;
+  for (char c: username) 
+  	s.insert(c);
+  int n = s.size();
+  n % 2 ? cout << "IGNORE HIM!" : cout << "CHAT WITH HER!";
+  cout << endl;
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  solve();
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

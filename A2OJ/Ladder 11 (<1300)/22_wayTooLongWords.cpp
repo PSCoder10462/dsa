bc637522ebedd6dcbf28afdc1f368d92cd9d6b4c
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-07-30 13:20:54 +0530

    Create 22_wayTooLongWords.cpp

---
+++ A2OJ/Ladder 11 (<1300)/22_wayTooLongWords.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) ((l+h)/2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define MOD 100'000'007
+#define mp make_pair
+#define endl '\n'
+
+void file_i_o() {
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+}
+
+void solve() {
+  string s;
+  cin >> s;
+  int n = s.length();
+  if (n <= 10) {
+  	cout << s << endl;
+  	return;
+  }
+  cout << s[0] << n-2 << s[n-1] << endl;
+}
+
+int32_t main () {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+#ifndef ONLINE_JUDGE
+clock_t end = clock();
+cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+<< " sec";
+#endif
+  return 0;
+}

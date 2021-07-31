70fdc756e02f06b20a3bb1d5951780898a042da4
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-07-31 14:26:40 +0530

    Create 28 team.cpp

---
+++ A2OJ/Ladder 11 (<1300)/28 team.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) ((l+h)/2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define MOD 100'000'007
+#define mp make_pair
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  int t;
+  cin >> t;
+  int problems = 0;
+  while (t--) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    bool one = a and b,
+         two = b and c,
+         thr = a and c;
+
+    if (one || two || thr) problems++;
+  }
+  cout << problems;
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  solve();
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

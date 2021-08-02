1fc9234ac15bd9925192e5808c8fa80a50b3f6d1
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-08-02 12:35:55 +0530

    Create 37 even odds.cpp

---
+++ A2OJ/Ladder 11 (<1300)/37 even odds.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define mid(l, h) ((l+h)/2)
+#define loop(i, a, b) for (int i = a; i <= b; ++i)
+#define looprev(i, a, b) for (int i = a; i >= b; --i)
+#define MOD 100'000'007
+#define mp make_pair
+#define endl '\n'
+
+void file_i_o() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  int no;
+  
+  // calculating odd numbers
+  if (n&1) {
+    no = (n+1)/2;
+  } else no = n/2;
+
+  if (k <= no) {
+    cout << 1 + (k-1)*2;
+    return;
+  } else {
+    k -= no;
+    cout << 2 + (k-1)*2;
+  }
+}
+
+int32_t main() {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  solve();
+#ifndef ONLINE_JUDGE
+  clock_t end = clock();
+  cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+       << " sec";
+#endif
+  return 0;
+}

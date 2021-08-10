1cd0abf1cd14109b64c82caa75c5fc34850d6010
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-08-10 07:29:34 +0530

    Create CHFINVNT.cpp

---
+++ codechef/long challenges/august 2021/CHFINVNT.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define v                 vector
+#define pb                push_back
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i <= b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define mp                make_pair
+#define endl              '\n'
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+
+void file_i_o() {
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+}
+
+void solve() {
+  ll n, k, p; cin >> n >> p >> k;
+  ll a = n/k, b = n%k;
+  ll rem = p%k;
+  ll days = (a+1)*(min(rem, b));
+  if (rem > b) days += (rem-b)*a;
+  p -= rem;
+  days += p/k;
+  cout << days+1 << endl;
+}
+
+int main () {
+  clock_t start = clock();
+  file_i_o();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll T;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+#ifndef ONLINE_JUDGE
+clock_t end = clock();
+cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+<< " sec";
+#endif
+  return 0;
+}

edfb373d1366328ebcc67b5c37df79b1f3415be3
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-11-20 13:33:29 +0530

    Create NEWPIECE.cpp

---
+++ codechef/starters/17/NEWPIECE.cpp
@@ -0,0 +1,52 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i <= b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int a, b, p, q; cin >> a >> b >> p >> q;
+    if (p == a and q == b) cout << 0;
+    else if (((p+q)&1) == ((a+b)&1)) cout << 2;
+    else cout << 1;
+    cout << endl;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    ll T;
+    cin >> T;
+    while (T--) {
+      solve();
+    }
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

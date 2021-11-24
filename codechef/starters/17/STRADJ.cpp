2a709a15128f17d0af788dee3edda95d191d3841
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-11-24 19:10:37 +0530

    Create STRADJ.cpp

---
+++ codechef/starters/17/STRADJ.cpp
@@ -0,0 +1,59 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i <= b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    ll n; cin >> n;
+    string s; cin >> s;
+    ll c1, c0; c1 = c0 = 0;
+    for (char c: s) 
+        c == '0' ? c0++ : c1++;
+    if (c1 == 0 or c0 == 0) {
+        cout << "Bob";
+    } else if (c1 == 1 or c0 == 1) {
+        cout << "Alice";
+    } else {
+        cout << (n&1 ? "Alice" : "Bob");
+    } cout << endl;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    ll T;
+    cin >> T;
+    while (T--) {
+      solve();
+    }
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

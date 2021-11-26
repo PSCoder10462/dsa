14268341a290dbd0e2b720e98aeb4027da31565a
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-11-27 01:25:27 +0530

    Create SUBMIMX.cpp

---
+++ codechef/FizzBuzz/SUBMIMX.cpp
@@ -0,0 +1,54 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i <= b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    ll n, m; cin >> n >> m;
+    ll z = n-m;
+    ll p = z/(m+1);
+    ll t = z%(m+1);
+    ll ans1 = t*(p+1)*(p+2)/2;
+    ll ans2 = (m+1-t)*(p*(p+1)/2);
+    cout << ans1 + ans2 << endl;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    ll T;
+    cin >> T;
+    while (T--) {
+      solve();
+    }
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

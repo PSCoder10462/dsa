304ad719cc024aba8538185c976e2d8407e8eb3b
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-11-27 19:18:27 +0530

    Create C.cpp

---
+++ ATCODER/NEC Programming Contest 2021(AtCoder Beginner Contest 229)/C.cpp
@@ -0,0 +1,58 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i <= b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    ll n, w; cin >> n >> w;
+    vector<pii> ch(n);
+    loop(i, 0, n-1) cin >> ch[i].ff >> ch[i].ss;
+    sort(ch.begin(), ch.end());
+    ll ans = 0;
+    looprev(i, n-1, 0) {
+        if (w >= ch[i].ss) {
+            ans += ch[i].ss*ch[i].ff;
+            w -= ch[i].ss;
+        } else {
+            ans += ch[i].ff * w;
+            break;
+        }
+    }
+    cout << ans;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

be502341ecaf69066be8f2f7fad1a75ad4e6baef
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-11-29 19:17:38 +0530

    Create CSUB.cpp

---
+++ codechef/practice/Easy/CSUB.cpp
@@ -0,0 +1,53 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i <= b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    ll n; string s; cin >> n >> s;
+    ll ctr = 0;
+    for (char c: s) 
+        ctr = c == '1' ? ctr+1 : ctr;
+
+    cout << ctr*(ctr+1)/2 << endl;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    ll T;
+    cin >> T;
+    while (T--) {
+      solve();
+    }
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

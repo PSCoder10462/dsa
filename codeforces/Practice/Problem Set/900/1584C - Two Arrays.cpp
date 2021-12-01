5c25d5ad8c1f7adad6c0c39003e3d702b8e12ac4
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-01 13:59:35 +0530

    Create 1584C - Two Arrays.cpp

---
+++ codeforces/Practice/Problem Set/900/1584C - Two Arrays.cpp
@@ -0,0 +1,60 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i <= b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n; cin>> n;
+    vi a(n), b(n);
+    for (auto &i: a) cin >> i;
+    for (auto &i: b) cin >> i;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    loop(i, 0, n-1) {
+        if (a[i] == b[i] or a[i]+1 == b[i]) continue;
+        else {
+            cout << "no" << endl; return;
+        }
+    }
+    cout << "yes" << endl;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    ll T;
+    cin >> T;
+    while (T--) {
+      solve();
+    }
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

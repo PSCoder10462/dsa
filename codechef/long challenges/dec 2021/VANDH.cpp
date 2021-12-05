c63530515009bb473ae43b8b9e07af870a7ebf00
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-05 18:28:12 +0530

    Create VANDH.cpp

---
+++ codechef/long challenges/dec 2021/VANDH.cpp
@@ -0,0 +1,81 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    ll n, m; cin >> n >> m;
+    string ans;
+
+    if (n <= m) {
+
+        loop(i, 0, n+1) 
+            ans += "10";
+
+        m -= n;
+
+        loop(i, 0, m-1)
+            ans += "110";
+
+        if (m)
+            ans.pb('1');
+
+    } else {
+
+        loop(i, 0, m+1) 
+            ans += "01";
+
+        n -= m;
+
+        loop(i, 0, n-1)
+            ans += "001";
+
+        if (n)
+            ans.pb('0');
+
+    }
+
+    cout << ans.length() << endl << ans << endl;
+
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    ll T;
+    cin >> T;
+    while (T--) {
+      solve();
+    }
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

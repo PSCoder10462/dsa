3b3e1596547a0d4ef9070430949fa1374ca70b0e
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-08 19:17:21 +0530

    Create 7 word.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/7 word.cpp
@@ -0,0 +1,68 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    const int diff = 'a'-'A';
+
+    int l, u; l=u=0;
+
+    string s; cin >> s;
+
+    for (auto i: s) {
+        if (i >= 'a' and i <= 'z') l++;
+        else u++;
+    }
+
+    if (l >= u) {
+        for (auto &i: s) {
+            if (i >= 'A' and i<= 'Z') 
+                i += diff;
+        }
+    } else {
+        for (auto &i: s) {
+            if (i >= 'a' and i<= 'z') 
+                i -= diff;
+        }
+    }
+
+    cout << s;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

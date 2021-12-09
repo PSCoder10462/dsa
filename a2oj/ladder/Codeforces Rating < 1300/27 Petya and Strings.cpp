f4f4e7b17ae6721dd74ef9f9fc3be9eb24acdd2a
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-09 15:00:10 +0530

    Create 27 Petya and Strings.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/27 Petya and Strings.cpp
@@ -0,0 +1,57 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    string a, b; cin >> a >> b;
+    transform(all(a), a.begin(), ::tolower);
+    transform(all(b), b.begin(), ::tolower);
+
+    int n = (int)a.length();
+
+    loop(i, 0, n) {
+        if (a[i] == b[i]) continue;
+        if (a[i] < b[i]) cout << -1;
+        else cout << 1; 
+        return;
+    }
+    cout << 0;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

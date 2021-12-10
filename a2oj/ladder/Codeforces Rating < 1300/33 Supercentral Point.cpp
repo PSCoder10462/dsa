6141a094fe63163a783d993460c8fc9b0680c5e8
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-11 01:08:32 +0530

    Create 33 Supercentral Point.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/33 Supercentral Point.cpp
@@ -0,0 +1,66 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n; cin >> n;
+    vector<pii> v(n);
+    for (auto &i: v) cin >> i.ff >> i.ss;
+
+    int ctr = 0;
+
+    loop(i, 0, n) {
+        bool up(false), down(false), left(false), right(false);
+        loop(j, 0, n) {
+            if (v[i].ff == v[j].ff) {
+                if (v[i].ss > v[j].ss) down = true;
+                if (v[i].ss < v[j].ss) up = true;
+            }
+            if (v[i].ss == v[j].ss) {
+                if (v[i].ff > v[j].ff) left = true;
+                if (v[i].ff < v[j].ff) right = true;
+            }
+        }
+        if (left and right and up and down) ctr++;
+    }
+
+    cout << ctr;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

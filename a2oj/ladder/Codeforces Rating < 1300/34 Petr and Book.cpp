09341c7cbcc8ef12b5baecaa7340b301ded5127e
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-11 18:57:52 +0530

    Create 34 Petr and Book.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/34 Petr and Book.cpp
@@ -0,0 +1,62 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n; cin >> n;
+    vi v(7);
+    int s = 0;
+    for (auto &i: v) {cin >> i; s+=i;}
+    n %= s;
+    if (!n) {
+        looprev(i, 6, 0) if (v[i]) {
+            cout << i+1; return;
+        }
+    }
+
+    loop(i, 0, 7) {
+        n -= v[i];
+        if (n <= 0) {
+            cout << i + 1;
+            return;
+        }
+    }
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

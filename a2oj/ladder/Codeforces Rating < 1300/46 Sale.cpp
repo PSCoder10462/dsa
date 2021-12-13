885bd857ed8fa29f0c68dfab4be6ac8b36d5e44d
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-13 15:40:23 +0530

    Create 46 Sale.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/46 Sale.cpp
@@ -0,0 +1,56 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vi v(n);
+    for (auto &i: v) cin >> i;
+    sort(all(v));
+
+    ll ans = 0, ctr = 0;
+    loop(i, 0, n) {
+        if (v[i] >=0) break;
+        if (v[i] < 0 and ctr <= m) {ans += v[i]; ctr++;}
+        if (ctr >= m) break;
+    }
+    cout << -ans;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

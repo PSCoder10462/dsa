ff63995ab7b62bba832cbb6ccbe0c3c4d2cca842
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-13 12:53:54 +0530

    Create 40 Jeff and Digits.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/40 Jeff and Digits.cpp
@@ -0,0 +1,71 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    
+    int n; cin >> n;
+
+    int f, z; f = z = 0;
+
+    loop(i, 0, n) {
+        int t; cin >> t;
+        if (t == 5)
+            f++;
+        else 
+            z++;
+    }
+
+    if (!z) {
+        cout<<-1; return;
+    }
+
+    f /= 9;
+
+    if (!f) {
+        cout << 0; return;
+    }
+
+    loop(i, 0, f) cout << 555555555;
+
+    loop(i, 0, z) cout << 0;
+
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

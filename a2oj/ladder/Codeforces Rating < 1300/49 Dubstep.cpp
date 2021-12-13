b24dc043136cb2be8669fd967385383cd988c62e
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-13 17:33:18 +0530

    Create 49 Dubstep.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/49 Dubstep.cpp
@@ -0,0 +1,59 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    string s; cin >> s;
+    int n = (int)s.length();
+    string op;
+    for (int i=0; i<n; ++i) {
+        if (s[i] == 'W' and i+2 < n) {
+            if (s[i+1] == 'U' and s[i+2] == 'B') {
+                i+=2;
+                if (op.length() > 0 and op[op.length()-1] != ' ') 
+                    op.pb(' ');
+                continue;
+            }
+        }
+        op.pb(s[i]);
+    }
+    cout << op;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

48954194fc98bd4e26f3202f67f52a8ef0c35ccb
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-15 15:48:06 +0530

    Create 67 Xenia and Divisors.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/67 Xenia and Divisors.cpp
@@ -0,0 +1,74 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    ll n; cin >> n;
+
+    vi f(8, 0);
+
+    loop(i, 0, n) {
+        int t; cin >> t;
+        f[t]++;
+    }
+
+    if (f[5] or f[7]) {
+        cout << -1; return;
+    }
+
+    if (f[1] == n/3 and f[2]+f[3] == n/3 and f[4]+f[6] == n/3
+        and f[3] <= f[6] and f[2] == f[4]+f[6]-f[3]
+        ) {
+
+        loop(i, 0, f[4])
+            cout << "1 2 4\n";
+
+        loop(i, 0, f[3])
+            cout << "1 3 6\n";
+
+        loop(i, 0, f[6]-f[3])
+            cout << "1 2 6\n";
+
+    }
+    else {
+        cout << -1; return;
+    }
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

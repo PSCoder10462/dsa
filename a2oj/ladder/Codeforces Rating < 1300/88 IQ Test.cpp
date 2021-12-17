256a3923e793cb84345991e3dc00cc8cdb75bb8f
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-17 16:15:22 +0530

    Create 88 IQ Test.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/88 IQ Test.cpp
@@ -0,0 +1,67 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    vector<vector<bool>> v(4, vector<bool>(4));
+    loop(i, 0, 4) 
+        loop(j, 0, 4) {
+            char c; cin >> c;
+            v[i][j] = c == '#';
+        }
+
+    loop(i, 0, 3) {
+        loop(j, i, 3) {
+            int t, f; t = f = 0;
+            loop(a, i, i+2) 
+                loop(b, j, j+2) {
+                    if (v[a][b]) 
+                        t++;
+                    else f++;
+                }
+            if (t >= 3 or f >= 3) {
+                cout << "YES"; return;
+            }
+        }
+    }
+
+    cout << "NO";
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

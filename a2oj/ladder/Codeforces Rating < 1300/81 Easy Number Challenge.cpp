220364e98077c2adc5a2f9888380d1be4eb9d38b
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-17 12:18:13 +0530

    Create 81 Easy Number Challenge.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/81 Easy Number Challenge.cpp
@@ -0,0 +1,70 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'073'741'824
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+ll get_div(int n) {
+    ll ctr = 0;
+    for (int i=1; i*i<= n; ++i) {
+        if (n%i == 0) {
+            if (n/i == i) ctr++;
+            else ctr += 2;
+        }
+    }
+    return ctr;
+}
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    ll ans = 0;
+
+    loop(i, 1, a+1) {
+        loop(j, 1, b+1) {
+            loop(k, 1, c+1) {
+                ans += (get_div(i*j*k)%mod);
+                ans %= mod;
+            }
+        }
+    }
+
+    cout << ans;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

ca2130c8a4dd33b2b15f58fd4e70b53ecb777514
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-17 18:26:41 +0530

    Create 91 T-primes.cpp

---
+++ a2oj/ladder/Codeforces Rating < 1300/91 T-primes.cpp
@@ -0,0 +1,65 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    const ll M = (ll)1e6+5;
+    vector<bool> pr(M, true);
+    pr[1] = false;
+    for (ll i=2; i*i < M; ++i) {
+        if (!i) continue;
+        for (ll j=2; j*i < M; ++j)
+            pr[i*j] = false;
+    }
+
+    ll n; cin >> n;
+
+    while (n--) {
+        ll t; cin >> t;
+        ll sr = (ll)sqrt(t);
+        if (sr*sr == t and pr[sr]) 
+            cout << "YES";
+        else 
+            cout << "NO";
+        cout << endl;
+    }
+
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

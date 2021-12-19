b1721fbc38b17a0b1602a729eaecc2500bebf8a7
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2021-12-19 22:41:51 +0530

    Create 18 Chips.cpp

---
+++ a2oj/ladder/1300 <= Codeforces Rating <= 1399/18 Chips.cpp
@@ -0,0 +1,57 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n, m; cin >> n >> m;
+    int f = n*(n+1)/2;
+    m %= f;
+
+    int lo = 0, hi = n+1;
+
+    while (lo != hi-1) {
+        int mi = mid(lo, hi);
+        if (mi*(mi+1)/2 <= m) lo = mi;
+        else hi = mi;
+    }
+
+    cout << m - lo*(lo+1)/2;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

31c82c3e017c6c61bc0bc3cabbd28a864741c537
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-01-06 13:26:49 +0530

    Create 57 Find Marble.cpp

---
+++ a2oj/ladder/1300 <= Codeforces Rating <= 1399/57 Find Marble.cpp
@@ -0,0 +1,58 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n, s, t; cin >> n >> s >> t;
+    vector<int> p(n);
+    for (auto &i: p) cin >> i;
+    int ctr = 0;
+    vector<bool> vis(n+1, false);
+    while (s != t) {
+        ctr++;
+        if (vis[s]) {
+            cout << -1; return;
+        }
+        vis[s] = true;
+        s = p[s-1];
+    }
+    cout << ctr;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

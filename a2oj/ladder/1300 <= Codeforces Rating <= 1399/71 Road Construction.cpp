0da0f49af0a8c61a4bd102f4f2cb85076417ed21
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-01-07 17:57:04 +0530

    Create 71 Road Construction.cpp

---
+++ a2oj/ladder/1300 <= Codeforces Rating <= 1399/71 Road Construction.cpp
@@ -0,0 +1,63 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n, m; cin >> n >> m;
+    unordered_set <int> s;
+    while (m--) {
+        int a, b; cin >> a >> b;
+        s.insert(a);
+        s.insert(b);
+    }
+    int e = 0;
+    loop(i, 1, n+1) {
+        if (s.find(i) == s.end()) {
+            e = i; break;
+        }
+    }
+    cout << n-1 << endl;
+    loop(i, 1, n+1) {
+        if (e != i) {
+            cout << e << ' ' << i << endl;
+        }
+    }
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

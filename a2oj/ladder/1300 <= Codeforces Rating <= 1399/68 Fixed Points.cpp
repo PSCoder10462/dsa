a8a121297ff1b417b3a67748d33b12c7ab6aa7c2
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-01-07 17:37:43 +0530

    Create 68 Fixed Points.cpp

---
+++ a2oj/ladder/1300 <= Codeforces Rating <= 1399/68 Fixed Points.cpp
@@ -0,0 +1,58 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n; cin >> n;
+    vector<int> v(n);
+    loop(i, 0, n) cin >> v[i];
+    int ctr = 0; bool flag = false;
+    loop(i, 0, n) {
+        if (v[i] == i) ctr++;
+        else {
+            if (v[v[i]] == i) flag = true;
+        }
+    }
+    if (ctr == n) cout << n;
+    else {
+        cout << (flag ? ctr+2 : ctr+1);
+    }
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

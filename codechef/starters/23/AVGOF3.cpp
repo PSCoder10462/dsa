e74b70a203c8d69ca2ac961eefc00d4515eff409
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-01-27 10:16:35 +0530

    Create AVGOF3.cpp

---
+++ codechef/starters/23/AVGOF3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int tt; cin >> tt;
+	while (tt--) {
+	    int x; cin >> x;
+	    cout << 1 << ' ' << x << ' ' << 2*x - 1 << '\n';
+	}
+	return 0;
+}

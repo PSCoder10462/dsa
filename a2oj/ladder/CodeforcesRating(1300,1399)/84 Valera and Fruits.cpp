4984dd28cda44ef54a7dfe99026f02220035e3b2
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-01-29 00:56:04 +0530

    Create 84 Valera and Fruits.cpp

---
+++ a2oj/ladder/CodeforcesRating(1300,1399)/84 Valera and Fruits.cpp
@@ -0,0 +1,65 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+const int INF = 3005;
+
+void solve() {
+    int n, v; cin >> n >> v;
+    vector<int> fruits(INF, 0);
+    loop(i, 0, n) {
+        int a, b; cin >> a >> b;
+        fruits[a] += b;
+    }
+    int ans = 0;
+    loop(i, 1, INF) {
+        int available = fruits[i-1]+fruits[i];
+        if (available >= v) {
+            ans += v;
+            fruits[i] -= max(v-fruits[i-1], 0);
+        } else {
+            ans += fruits[i-1]+fruits[i];
+            fruits[i] = fruits[i-1] = 0;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

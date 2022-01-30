0488220e736b9edb805b26d34ad07018995c91a6
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-01-30 13:05:24 +0530

    Create PREZ (better).cpp

---
+++ codechef/lunchtime/jan 2022/PREZ (better).cpp
@@ -0,0 +1,59 @@
+// TC: O(N)
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    ll n, k; cin >> n >> k;
+    string s; cin >> s;
+    ll ans = 0;
+    for (auto &i: s) {
+        ll req = (10-(i-'0')) % 10;
+        if (req > k) break;
+        ans++;
+        k = ((k-req)/10)*10 + req; // getting a number < k and unit place = reqd
+    }
+    cout << ans << endl;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    ll T;
+    cin >> T;
+    while (T--) {
+      solve();
+    }
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

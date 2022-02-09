9148f78736835f23032903a0e31cc179e21520d0
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-02-09 15:28:31 +0530

    Create 89 Ciel and Flowers.cpp

---
+++ a2oj/ladder/CodeforcesRating(1300,1399)/89 Ciel and Flowers.cpp
@@ -0,0 +1,60 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    vector<int> v(3);
+    for (auto &i: v) cin >> i;
+    int ans = 0;
+    loop(i, 0, 3) {
+        int temp = 0;
+        bool flag = true;
+        for (auto f: v) {
+            if (f < i) {
+                flag = false; break;
+            }
+            temp += (f-i)/3;   
+        }
+        if (flag)
+            ans = max(ans, temp+i);
+    }
+    cout << ans;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

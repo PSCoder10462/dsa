1b546853a96bd44d83d942c05a70f5e9ea901fa8
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-02-10 23:36:36 +0530

    Create 93 Exams.cpp

---
+++ a2oj/ladder/CodeforcesRating(1300,1399)/93 Exams.cpp
@@ -0,0 +1,56 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n; cin >> n;
+    vector<pair<int, int>> v(n);
+    for (auto &i: v) cin >> i.ff >> i.ss;
+    sort(all(v));
+    int ans = v[0].ss;
+    loop(i, 1, n) {
+        if (v[i].ss >= ans) 
+            ans = v[i].ss;
+        else 
+            ans = v[i].ff;
+    } 
+    cout << ans;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

5bb76d490823d08110d3d4677d0a39fabf1d3183
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-02-15 17:14:16 +0530

    Create 28 Heads or Tails.cpp

---
+++ a2oj/ladder/CodeforcesRating(1400,1499)/28 Heads or Tails.cpp
@@ -0,0 +1,57 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int x, y, a, b; cin >> x >> y >> a >> b;
+    // v-> x
+    // p -> y
+    vector<pair<int, int>> ans;
+    loop(i, b, y+1) {
+        if (x <= i) break;
+        loop(j, max(a, i+1), x+1) {
+            ans.pb(mp(j, i));
+        }
+    }   
+    sort(all(ans));
+    cout << ans.size() << endl;
+    for (auto const &i: ans) cout << i.ff << ' ' << i.ss << endl;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

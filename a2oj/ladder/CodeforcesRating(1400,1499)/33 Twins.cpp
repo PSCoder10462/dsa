a705712ea720c673e2b9c1d507e8e39bc254cc1d
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-02-15 18:32:22 +0530

    Create 33 Twins.cpp

---
+++ a2oj/ladder/CodeforcesRating(1400,1499)/33 Twins.cpp
@@ -0,0 +1,62 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+const int INF = 1e5+5;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> v(n);
+    int req = 0;
+    for (auto &i: v) {
+        cin >> i;
+        req += i;
+    }    
+    req /= 2;
+    sort(all(v));
+    int coins = 0;
+    while (coins < n) {
+        req -= v[n-1-coins];
+        coins++;
+        if (req < 0) break;   
+    }
+    cout << coins;
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

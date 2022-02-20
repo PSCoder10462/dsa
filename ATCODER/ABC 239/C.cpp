be4bda5a386260ad987eaae7de37870f2d0721df
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-02-20 13:09:03 +0530

    Create C.cpp

---
+++ ATCODER/ABC 239/C.cpp
@@ -0,0 +1,64 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    ll x1, x2, y1, y2; cin >> x1 >> y1 >> x2 >> y2;
+    vector<pii> v {
+        {1, 2},
+        {1, -2},
+        {2, 1},
+        {2, -1},
+        {-1, 2},
+        {-1, -2},
+        {-2, 1},
+        {-2, -1}
+    };
+    for (auto [x, y]: v) {
+        x += x1;
+        y += y1;
+        ll dist = (x-x2)*(x-x2)+(y-y2)*(y-y2);
+        if (dist == 5) {
+            cout << "Yes"; return;
+        }
+    }
+    cout << "No";
+}
+
+int main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

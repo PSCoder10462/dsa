e2c3f809d3fd159bbfd1a53cebfa5031ced8317f
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-03-25 17:55:52 +0530

    Create Sum of Two Values.cpp

---
+++ CSES/Sorting and Searching/Sum of Two Values.cpp
@@ -0,0 +1,68 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define int               long long
+#define vi                vector<int>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<int, int>
+#define pb                push_back
+#define sz(container)     (container.size()) 
+#define mid(l, h)         (l+(h-l)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n, x; cin >> n >> x;
+    
+    map<int, vi> ind;
+    
+    loop(i, 0, n) {
+        int t; cin >> t;
+        ind[t].pb(i+1);
+    }
+    
+    for (auto [num, vec] : ind) {
+        int req = x - num;
+        if (sz(vec) == 0 or req <= 0) continue;
+        
+        if (req == num) {
+            if (sz(vec) > 1) {
+                cout << vec[0] << ' ' << vec[1]; return;
+            } else continue;
+        }
+        
+        if (sz(ind[req]) > 0) {
+            cout << vec[0] << ' ' << ind[req][0]; return;
+        }
+    }
+    cout << "IMPOSSIBLE";
+}
+
+int32_t main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

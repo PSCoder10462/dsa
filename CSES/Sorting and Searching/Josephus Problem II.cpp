7114a23d44fcfff3a4edb3a09e6d6d865fd00420
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-03-26 23:29:24 +0530

    Create Josephus Problem II.cpp

---
+++ CSES/Sorting and Searching/Josephus Problem II.cpp
@@ -0,0 +1,64 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+// #define int               long long
+#define vi                vector<int>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<int, int>
+#define pb                push_back
+#define sz(container)     (container.size()) 
+#define mid(l, h)         (l+(h-l)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+  
+/******************************************************/
+
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n, k; cin >> n >> k;
+    ordered_set<int> s;
+    loop(i, 1, n+1) s.insert(i);
+    int pos = 0;
+    while (sz(s)) {
+        pos %= sz(s);
+        pos = (pos+k)%sz(s);
+        auto it = s.find_by_order(pos);
+        cout << *it << ' ';
+        s.erase(it);
+    }
+}
+
+int32_t main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

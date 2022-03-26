06649bae229a9d5520eb0bf152c2d1a5318f951d
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-03-26 11:46:48 +0530

    Create Playlist.cpp

---
+++ CSES/Sorting and Searching/Playlist.cpp
@@ -0,0 +1,58 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define int               long long
+#define vi                vector<int>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<int, int>
+#define pb                push_back
+#define sz(container)     (container.size()) 
+#define mid(l, h)         (l+(h-l)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n; cin >> n;
+    vi v(n);
+    for (auto &i: v) cin >> i;
+    set<int> s;
+    int ans = 0;
+    for (int i=0,j=0; i<n; ++i) {
+        while (j < n and !s.count(v[j])) {
+            s.insert(v[j]);
+            j++;
+        }
+        ans = max(ans, j-i);
+        s.erase(v[i]);
+    }
+    cout << ans;
+}
+
+int32_t main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

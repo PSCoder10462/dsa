d13821a49f97543240e273c1f9e82079df1bd815
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-03-26 13:55:05 +0530

    Create Josephus Problem I.cpp

---
+++ CSES/Sorting and Searching/Josephus Problem I.cpp
@@ -0,0 +1,62 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define int               long long
+#define vi                vector<int>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<int, int>
+#define pb                push_back
+#define sz(container)     (container.size()) 
+#define mid(l, h)         (l+(h-l)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pinf              LLONG_MAX
+#define ninf              LLONG_MIN
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void incr(auto &it, set<int> &s) {
+    ++it;
+    if (it == s.end()) it = s.begin();
+}
+
+void solve() {
+    int n; cin >> n;
+    set<int> s;
+    loop(i, 1, n+1) s.insert(i);
+    auto it = s.begin();
+    incr(it, s);
+    while (sz(s)) {
+        cout << *it << ' ';
+        auto temp = it;
+        incr(it, s);
+        s.erase(temp);
+        if (!sz(s)) break;
+        incr(it, s);
+    }    
+}
+
+int32_t main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+    << " sec";
+#endif
+    return 0;
+}

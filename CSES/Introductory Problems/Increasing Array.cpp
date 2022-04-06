c521045b6c94127c273fe0ab40724ef22bcbcfbd
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-04-06 16:19:56 +0530

    Create Increasing Array.cpp

---
+++ CSES/Introductory Problems/Increasing Array.cpp
@@ -0,0 +1,53 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define int               long long
+#define vi                vector<int>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<int, int>
+#define pb                push_back
+#define sz(container)     (container.size()) 
+#define mid(l, h)         (l+(h-l)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+void solve() {
+    int n; cin >> n;
+    vi v(n);
+    for (auto &i: v) cin >> i;
+    int ans = 0;
+    loop(i, 1, n) {
+        ans += max(0ll, v[i-1]-v[i]);
+        v[i] = max(v[i], v[i-1]);
+    }
+    cout << ans;
+}
+
+int32_t main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << 
+        (double)(end - start) / double(CLOCKS_PER_SEC)
+        << " sec";
+#endif
+    return 0;
+}

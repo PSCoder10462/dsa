59611acf1f2f4d26ae048eb397a0b93dad2d8503
Parv Sharma <59911189+PSCoder10462@users.noreply.github.com>
2022-04-06 16:10:46 +0530

    Create Missing Number.cpp

---
+++ CSES/Introductory Problems/Missing Number.cpp
@@ -0,0 +1,56 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define int               long long
+#define vi                vector<int>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<int, int>
+#define pb                push_back
+#define sz(container)     (container.size()) 
+#define mid(l, h)         (l+(h-l)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+void solve() {
+    int n; cin >> n;
+    vector<bool> v(n+10, false); 
+    loop(i, 1, n) {
+        int t; cin >> t;
+        v[t] = true;
+    }
+    loop(i, 1, n+10) {
+        if (!v[i]) {
+            cout << i;
+            break;
+        }
+    }
+}
+
+int32_t main () {
+    clock_t start = clock();
+    file_i_o();
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << 
+        (double)(end - start) / double(CLOCKS_PER_SEC)
+        << " sec";
+#endif
+    return 0;
+}
